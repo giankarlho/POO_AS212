d1031045e2efb59605e43beeebdac9ad43fc8813
Giancarlo Valencia Sotomayor <gvalencia@vallegrande.edu.pe>
2022-04-01 08:44:49 -0500

    cambio

--- src/algoritmos/Calculadora1.java
+++ src/algoritmos/Calculadora1.java
@@ -1,20 +1,29 @@
 package algoritmos;
 
+import java.util.Scanner;
+
 public class Calculadora1 {
     
     public static void main(String[] args) {
-        double nro1 = 0.0, nro2=0.0, resultado;
+        double nro1 = 0.0, nro2=0.0, resultado=0.0;
         nro1 = (int) (Math.random()*(100-1)) + 1;
-        nro2 = (int) (Math.random()*(100-1)) + 1;
-        char operador = 0;
-        System.out.println("Ing. el operador: '+'  '-' '*' '/' ");
+        nro2 = (int) (Math.random()*(100-1)) + 1;        
+        System.out.println("Ing. el operador '+'  '-' '*' '/' :");
+        Scanner ingreso = new Scanner(System.in);
+        char operador = ingreso.next().charAt(0);
         switch(operador){
             case '+': resultado = nro1 + nro2; break;
             case '-': resultado = nro1 - nro2; break;
             case '*': resultado = nro1 * nro2; break;
             case '/': resultado = nro1 / nro2; break;
         }
-        
+        System.out.println("El resultado es: " + resultado);
+    }
+    
+}
+
+
+
 //        if (operador =='+'){
 //            resultado = nro1 + nro2;
 //        } else if (operador =='-'){
@@ -24,7 +33,3 @@ public class Calculadora1 {
 //        } else if (operador =='/'){
 //            resultado = nro1 / nro2;
 //        }
-
-    }
-    
-}
